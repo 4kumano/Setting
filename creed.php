34bd203a162c87d38264bb1a216e4e61efaffdf7
4kumano <54392161+4kumano@users.noreply.github.com>
2019-09-29 02:51:41 +0800

    Update creed.php

--- creed.php
+++ creed.php
@@ -59,7 +59,7 @@ $dataMB = '{"type":"20","coin":1,"sign":"b839b94541448214601f29775659964c","requ
 $AuthMB = "aeeee02b2bdb4a4eb280f9ff876856b5";
 
 //Pivot
-$dataPv = '{"h_av":"2.0.3","h_dt":2,"h_os":27,"h_model":"Redmi 5","h_did":"376938074409627_07:c5:04","h_nt":1,"h_m":"5baceeb002b8755ffaf7b4d8","h_ch":"GooglePlay","h_ts":1569608170946,"h_app":"pivot","session":"9ce3daaf-68af-45e3-a756-7372967e3c45","h_language":"en","h_region":"US","h_sy":970,"h_zone_offset":28800,"h_zone_name":"Asia\/Makassar","h_zone_abbreviation":"GMT+08:00","h_carrier":"Smartfren,id,51028","h_sign":"c0c99629475b9fb2","theme":"1"}';
+$dataPv = '{"h_av":"2.0.3","h_dt":2,"h_os":27,"h_model":"Redmi 5","h_did":"376938074409627_07:c5:04","h_nt":1,"h_m":"5baceeb002b8755ffaf7b4d8","h_ch":"GooglePlay","h_ts":1569696577910,"h_app":"pivot","session":"9ef8629e-485c-4b9d-a6b1-818b3cbb7f03","h_language":"en","h_region":"US","h_sy":748,"h_zone_offset":28800,"h_zone_name":"Asia\/Makassar","h_zone_abbreviation":"GMT+08:00","h_carrier":"IND TELKOMSEL,id,51028","h_sign":"4c4021265282e61f","theme":"1"}';
 
 //coinpayu
 $tokenCP = "8b9c30eddb44839f3fdbdf63c0240a04f57f7870";
