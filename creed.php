8d42a61ef429f999316933aaa25edfdfdc4dc54d
4kumano <54392161+4kumano@users.noreply.github.com>
2019-10-29 02:16:02 +0800

    Update creed.php

--- creed.php
+++ creed.php
@@ -60,8 +60,8 @@ $AuthMB = "aeeee02b2bdb4a4eb280f9ff876856b5";
 $dataPv = '{"h_av":"2.0.3","h_dt":2,"h_os":27,"h_model":"Redmi 5","h_did":"376938074409627_07:c5:04","h_nt":1,"h_m":"5baceeb002b8755ffaf7b4d8","h_ch":"GooglePlay","h_ts":1569696577910,"h_app":"pivot","session":"9ef8629e-485c-4b9d-a6b1-818b3cbb7f03","h_language":"en","h_region":"US","h_sy":748,"h_zone_offset":28800,"h_zone_name":"Asia\/Makassar","h_zone_abbreviation":"GMT+08:00","h_carrier":"IND TELKOMSEL,id,51028","h_sign":"4c4021265282e61f","theme":"1"}';
 
 //coinpayu
-$tokenCP = "f9913383ee6678a703714a52ae5e55413cbf3d1a";
-$PHPSESSIDCP = "PHPSESSID=7jmcqkplme4dqjd9e71u2jqeme";
+$tokenCP = "c847237ed9745493e6c1cf634af73fb4fe64d340";
+$PHPSESSIDCP = "PHPSESSID=6pesb2pqjln9t2nclcsks3a5h4";
 $cfduidCP = "__cfduid=d4aec429a7cef137926fdeefc926d48311571510541";
 $useragentCP = "Mozilla/5.0 (Linux; Android 8.1.0; Redmi 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.99 Mobile Safari/537.36";
 
